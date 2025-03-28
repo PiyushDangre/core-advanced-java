a5f7b5beef7749ac0afc04f082eb6d1625d355d4
PI20216966 <PI20216966@L-156307485.wipro.com>
2025-03-28 14:09:11 +0530

    Renamed packge core to stream

--- java-multithreading-examples/src/main/java/com/stream/_01_Stream_Problem.java
+++ java-multithreading-examples/src/main/java/com/stream/_01_Stream_Problem.java
@@ -1,4 +1,4 @@
-package com.core;
+package com.stream;
 
 import java.util.Arrays;
 import java.util.Iterator;
--- java-multithreading-examples/src/main/java/com/stream/_02_Stream_Practice_Problems.java
+++ java-multithreading-examples/src/main/java/com/stream/_02_Stream_Practice_Problems.java
@@ -1,4 +1,4 @@
-package com.core;
+package com.stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
