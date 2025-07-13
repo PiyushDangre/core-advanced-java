6c59a2f44c8d173e7210c3503572209f50dd7845
Piyush Dangre <piyushdangre11@gmail.com>
2025-07-14 00:47:25 +0530

    firstNonRepeatedCharacter() program added in stream practice problems

--- java-multithreading-examples/src/main/java/com/stream/_02_Stream_Practice_Problems.java
+++ java-multithreading-examples/src/main/java/com/stream/_02_Stream_Practice_Problems.java
@@ -1,14 +1,6 @@
 package com.stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -21,7 +13,7 @@ import java.util.stream.Stream;
 public class _02_Stream_Practice_Problems {
 
 	public static void main(String[] args) {
-		
+
 		sumOfEvenNos();
 		calculateAvg();
 		nosOfStringsStartingWith("P");
@@ -38,54 +30,52 @@ public class _02_Stream_Practice_Problems {
 		secondLargestNumberinArray();
 		reverseEachWordOfString();
 		groupByAndMax();
+		firstNonRepeatedCharacter();
 	}
-	
-	
 
-	
+
 	/**
 	 * Group by common property and find max salary
-	 * 
+	 * <p>
 	 * - Here we are grouping by common age and finding max salary per group
-	 * 
 	 */
 	private static void groupByAndMax() {
 		List<Person> people = new ArrayList<Person>();
 		people.add(new Person(30, "Piyush", "Dangre", 30000));
 		people.add(new Person(30, "Aprajita", "Murthy", 40000));
 		people.add(new Person(2, "Daichi", "Sawamura", 15000));
-		
+
 		Map map = people.stream().collect(
 				Collectors.groupingBy(p -> p.getAge(), Collectors.maxBy(Comparator.comparingInt(p -> p.getSalary()))));
-		
+
 		System.out.println(map); // {2=Optional[Person [age=2, firstName=Daichi, lastName=Sawamura]], 30=Optional[Person [age=30, firstName=Aprajita, lastName=Murthy]]}
 
 	}
 
 	/**
 	 * Reverse each word of string
-	 * 
+	 * <p>
 	 * - First generate stream of words using Arrays.stream(s.split(" "))
 	 * - Then map to stringBuilder and reverse it
 	 * - Then collect using Collectors.collect(" ")
 	 */
 	private static void reverseEachWordOfString() {
 
-        String str = "Java Concept Of The Day";
-        String listOfReversed = Arrays.stream(str.split(" ")).map(s -> new StringBuilder(s).reverse().toString()).collect(Collectors.joining(" "));
-		System.out.println("String with reversed words = "+listOfReversed); // String with reversed words = avaJ tpecnoC fO ehT yaD
+		String str = "Java Concept Of The Day";
+		String listOfReversed = Arrays.stream(str.split(" ")).map(s -> new StringBuilder(s).reverse().toString()).collect(Collectors.joining(" "));
+		System.out.println("String with reversed words = " + listOfReversed); // String with reversed words = avaJ tpecnoC fO ehT yaD
 	}
 
 	/**
 	 * Find second largest number in an integer array?
-	 * 
-	 * 	- We have used mapToObj after generating IntStream using Arrays.stream(). 
-	 * 		-- This was done to convert int into Integer object to apply .sorted() method with Comparator.reverseOrder() as parameter.
-	 * 	- We have used skip(1) to skip first element after reverse ordering.
-	 * 	- we have used findFirst() to get the second element after skipping first.
+	 * <p>
+	 * - We have used mapToObj after generating IntStream using Arrays.stream().
+	 * -- This was done to convert int into Integer object to apply .sorted() method with Comparator.reverseOrder() as parameter.
+	 * - We have used skip(1) to skip first element after reverse ordering.
+	 * - we have used findFirst() to get the second element after skipping first.
 	 */
 	private static void secondLargestNumberinArray() {
-		int[] array = { 45, 12, 56, 15, 24, 75, 31, 89 };
+		int[] array = {45, 12, 56, 15, 24, 75, 31, 89};
 		Optional<Integer> secondLargets = Arrays.stream(array).mapToObj(i -> (Integer) i)
 				.sorted(Comparator.reverseOrder()).skip(1).findFirst();
 		System.out.println("Second largest integer is " + secondLargets.get()); // Second largest integer is 75
@@ -109,64 +99,64 @@ public class _02_Stream_Practice_Problems {
 	 * - Notice usage of .toArray() terminal method
 	 */
 	private static void mergeUnsortedArray() {
-		int[] a = new int[] { 4, 2, 5, 1 };
-		int[] b = new int[] { 8, 1, 9, 5 };
+		int[] a = new int[]{4, 2, 5, 1};
+		int[] b = new int[]{8, 1, 9, 5};
 		int[] ab = IntStream.concat(Arrays.stream(a), Arrays.stream(b)).sorted().distinct().toArray();
 		System.out.println("Sorted and de-duplicated array - " + Arrays.toString(ab)); // Sorted and de-duplicated array
 	}
 
 	/**
 	 * Given a list of strings, join the strings with ‘[‘ as prefix, ‘]’ as suffix and ‘,’ as delimiter?
-	 * 
+	 * <p>
 	 * - Here we are using Collectors.joining() method. It always returns String.
 	 */
 	private static void joinStringsSuffixPrefix() {
-        List<String> listOfStrings = Arrays.asList("Facebook", "Twitter", "YouTube", "WhatsApp", "LinkedIn");
-        String s = listOfStrings.stream().collect(Collectors.joining(",", "[", "]"));
-        System.out.println("Joined String - "+s);
+		List<String> listOfStrings = Arrays.asList("Facebook", "Twitter", "YouTube", "WhatsApp", "LinkedIn");
+		String s = listOfStrings.stream().collect(Collectors.joining(",", "[", "]"));
+		System.out.println("Joined String - " + s);
 	}
 
 	/**
 	 * Implement a method to partition a list into two groups based on a predicate using Java streams:
-	 * 
+	 * <p>
 	 * Source - https://www.geeksforgeeks.org/collectors-partitioningby-method-in-java/
 	 */
 	private static void evenOddPartitioning() {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-		
+
 		Map map = numbers.stream().collect(Collectors.partitioningBy(s -> s % 2 == 0));
-		
+
 		List<Integer> evenNos = (List<Integer>) map.get(true);
 		List<Integer> oddNos = (List<Integer>) map.get(false);
 
-		System.out.println("Even numbers list --> "+evenNos);
-		System.out.println("Odd numbers list --> "+oddNos);
-		
+		System.out.println("Even numbers list --> " + evenNos);
+		System.out.println("Odd numbers list --> " + oddNos);
+
 	}
 
 	/**
 	 * Given a list of strings, find the frequency of each word using Java streams:
 	 */
 	private static void wordFrequency() {
-		String[] strArray = {"apple", "banana", "apple", "cherry", 
-                "banana", "apple"};
-		
+		String[] strArray = {"apple", "banana", "apple", "cherry",
+				"banana", "apple"};
+
 		Map map = Arrays.stream(strArray).collect(Collectors.groupingBy(s -> s, Collectors.counting()));
-		
-		System.out.println("Word frequency map 1 ==> "+map); // Word frequency map ==> {banana=2, cherry=1, apple=3}
+
+		System.out.println("Word frequency map 1 ==> " + map); // Word frequency map ==> {banana=2, cherry=1, apple=3}
 
 		/**
 		 * - Below is another way of turning string into stream of letters.
 		 * - Here the difference is that string has spaces. So we have to use flatmap to convert to individual letters
 		 *   after splitting using " " (space) delimiter.
 		 */
-		
+
 		String inputString = "Java Concept Of The Day";
-		
+
 		Map map2 = Arrays.stream(inputString.toLowerCase().split(" ")).flatMap(s -> Arrays.stream(s.split("")))
 				.collect(Collectors.groupingBy(s -> s, Collectors.counting()));
-		
-		System.out.println("Word frequency map2 =>"+map2); // Word frequency map2 =>{p=1, a=3, c=2, d=1, t=2, e=2, f=1, v=1, h=1, y=1, j=1, n=1, o=2}
+
+		System.out.println("Word frequency map2 =>" + map2); // Word frequency map2 =>{p=1, a=3, c=2, d=1, t=2, e=2, f=1, v=1, h=1, y=1, j=1, n=1, o=2}
 	}
 
 	/**
@@ -178,12 +168,12 @@ public class _02_Stream_Practice_Problems {
 		 * Arrays.asList(array).stream().mapToInt(s -> Integer.valueOf(s)) .sorted()
 		 * .limit(3) .max();
 		 */
-		
+
 		OptionalInt thirdSmallest = Arrays.stream(array)
-		 .sorted()
-		 .limit(3)
-		 .max();
-		
+				.sorted()
+				.limit(3)
+				.max();
+
 		System.out.println("third smallest elemement = " + thirdSmallest.getAsInt()); // third smallest elemement = 3
 	}
 
@@ -192,41 +182,41 @@ public class _02_Stream_Practice_Problems {
 	 */
 	private static void sumOfTxnsForEachDay() {
 		List<Transaction> transactions = Arrays.asList(
-			    new Transaction("2022-01-01", 100),
-			    new Transaction("2022-01-01", 200),
-			    new Transaction("2022-01-02", 300),
-			    new Transaction("2022-01-02", 400),
-			    new Transaction("2022-01-03", 500)
-			);
-		
+				new Transaction("2022-01-01", 100),
+				new Transaction("2022-01-01", 200),
+				new Transaction("2022-01-02", 300),
+				new Transaction("2022-01-02", 400),
+				new Transaction("2022-01-03", 500)
+		);
+
 		// My first attempt way
-		
+
 		transactions.stream().collect(Collectors.groupingBy(s -> s.date)).entrySet().stream().forEach((s) -> {
 			String key = s.getKey();
 			Integer sum = s.getValue().stream().mapToInt(t -> t.amt).sum();
 			System.out.println("Key - " + key + " and sum - " + sum);
-		});		
-		
-		
+		});
+
+
 		/**
 		 * The above code prints -
 		 * 	Key - 2022-01-03 and sum - 500
-			Key - 2022-01-01 and sum - 300
-			Key - 2022-01-02 and sum - 700
+		 Key - 2022-01-01 and sum - 300
+		 Key - 2022-01-02 and sum - 700
 		 */
-		
-		
+
+
 		// Another refined way. Creates map of key -> date string , value -> Full IntSummaryStatistics object
-		
-		Map map = transactions.stream().collect(Collectors.groupingBy(s -> s.date , Collectors.summarizingInt(s -> s.amt)));
-		
+
+		Map map = transactions.stream().collect(Collectors.groupingBy(s -> s.date, Collectors.summarizingInt(s -> s.amt)));
+
 		System.out.println(map); // Creates map of key -> date string , value -> Full IntSummaryStatistics object which has sum
-		
+
 		// Another refined way. Creates map of key -> date string , value -> Integer Sum
 		// In above example .summarizingInt() is used. Here .summingInt() is used. Note the difference in names and the data returned.
 
-		Map map2 = transactions.stream().collect(Collectors.groupingBy(s -> s.date , Collectors.summingInt(s -> s.amt)));
-		
+		Map map2 = transactions.stream().collect(Collectors.groupingBy(s -> s.date, Collectors.summingInt(s -> s.amt)));
+
 		System.out.println(map2);
 	}
 
@@ -239,35 +229,36 @@ public class _02_Stream_Practice_Problems {
 		List<Integer> concatenatedSortedList = Stream.concat(list1.stream(), list2.stream()).sorted().collect(Collectors.toList());
 		System.out.println(concatenatedSortedList);
 	}
-	
-	
+
+
 	/**
 	 * Sort in ascending and descending order
 	 */
 	private static void sort() {
-	    List < String > colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
-	    List < String > colorsSortedAsc = colors.stream().sorted((s1, s2) -> s1.compareTo(s2)).collect(Collectors.toList());
-		System.out.println("Asc order => "+colorsSortedAsc);
-	    List < String > colorsSortedDesc = colors.stream().sorted((s1, s2) -> s2.compareTo(s1)).collect(Collectors.toList());
-	    System.out.println("Desc order => "+colorsSortedDesc);
-	    
-	    /**
-	     * - Another way of reverse ordering is using Comparator.reverseOrder()
-	     */
-	    
-	    List<String> colorsReversed = colors.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-	    System.out.println("Desc order of colors using Comparator.reverseOrder() - "+colorsReversed); // Desc order of colors using Comparator.reverseOrder() - [Red, Pink, Green, Brown, Blue]
+		List<String> colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
+		List<String> colorsSortedAsc = colors.stream().sorted((s1, s2) -> s1.compareTo(s2)).collect(Collectors.toList());
+		System.out.println("Asc order => " + colorsSortedAsc);
+		List<String> colorsSortedDesc = colors.stream().sorted((s1, s2) -> s2.compareTo(s1)).collect(Collectors.toList());
+		System.out.println("Desc order => " + colorsSortedDesc);
+
+		/**
+		 * - Another way of reverse ordering is using Comparator.reverseOrder()
+		 */
+
+		List<String> colorsReversed = colors.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+		System.out.println("Desc order of colors using Comparator.reverseOrder() - " + colorsReversed); // Desc order of colors using Comparator.reverseOrder() - [Red, Pink, Green, Brown, Blue]
 	}
 
 	/**
 	 * Write a Java program to count the number of strings in a list that start with a specific letter using streams.
+	 *
 	 * @param string
 	 */
 	private static void nosOfStringsStartingWith(String string) {
-		List < String > colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
+		List<String> colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
 		Long count = colors.stream().filter(s -> s.startsWith(string)).count();
-		System.out.println("Nos of strings starting with letter "+string+" is "+count);
-		
+		System.out.println("Nos of strings starting with letter " + string + " is " + count);
+
 	}
 
 	/**
@@ -281,28 +272,28 @@ public class _02_Stream_Practice_Problems {
 	}
 
 
-/**
- * Find the sum of all even numbers in a list of integers.
- */
-	private static void sumOfEvenNos() {
-		
-		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-		Integer sum = list.stream().filter(s -> s % 2 == 0).mapToInt(s -> s.intValue()).sum();
-		System.out.println("Sum of even numbers is "+sum); // Sum of even numbers is 30
-		
-	}
-	
 	/**
-	 * - here we are sorting collections on the basis of more than one keys. 
+	 * Find the sum of all even numbers in a list of integers.
+	 */
+	private static void sumOfEvenNos() {
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		Integer sum = list.stream().filter(s -> s % 2 == 0).mapToInt(s -> s.intValue()).sum();
+		System.out.println("Sum of even numbers is " + sum); // Sum of even numbers is 30
+
+	}
+
+	/**
+	 * - here we are sorting collections on the basis of more than one keys.
 	 * - We are using comparator.compare() method to generate custom comparator on the basis of keys(fields) that we pass.
 	 */
 	private static void compareCollections() {
-		
+
 		List<Person> people = new ArrayList<Person>();
 		people.add(new Person(30, "Piyush", "Dangre"));
 		people.add(new Person(30, "Aprajita", "Murthy"));
 		people.add(new Person(2, "Daichi", "Sawamura"));
-		
+
 		List<String> list1 = people.stream().sorted((p1, p2) -> Integer.compare(p1.getAge(), p2.getAge()))
 				.map(p -> p.getFirstName()).collect(Collectors.toList());
 		System.out.println(list1); // [Daichi, Piyush, Aprajita]
@@ -315,10 +306,30 @@ public class _02_Stream_Practice_Problems {
 				.sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getFirstName))
 				.map(p -> p.getFirstName()).collect(Collectors.toList());
 		System.out.println(list3); // [Daichi, Aprajita, Piyush]
-		
+
 	}
-	
-	
+
+
+	/**
+	 * Find the first non repeated character in the string
+	 */
+	private static void firstNonRepeatedCharacter() {
+
+		String input = "Java articles are Awesome";
+
+		System.out.println("First Non Repeated character in the string is -> ");
+
+		Arrays.stream(input.replace(" ", "").split(""))
+				.collect(Collectors.groupingBy(s -> s, () -> new LinkedHashMap<>(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter((e) -> e.getValue() == 1)
+				.findFirst()
+				.map((e)-> e.getKey())
+				.ifPresent(System.out::printf);
+
+
+	}
 
 }
 
