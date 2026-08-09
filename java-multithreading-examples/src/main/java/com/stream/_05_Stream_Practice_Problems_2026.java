cb9b8289177e14a7b32624be5cba26432ee595b5
Piyush Dangre <piyushdangre11@gmail.com>
2026-08-10 00:10:09 +0530

    New file added for stream API practice problems

---
+++ java-multithreading-examples/src/main/java/com/stream/_05_Stream_Practice_Problems_2026.java
@@ -0,0 +1,99 @@
+package com.stream;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class _05_Stream_Practice_Problems_2026 {
+
+    public static void main(String[] args) {
+        findMax();
+        findMaxWithoutSortingAndInbuiltMaxFunction();
+        countStringsWithSpecificPrefix();
+        findFirstNonRepeatedChatracterInString();
+        sumOfNumbersInList();
+        findDuplicates();
+        groupByLength();
+        concatenateStrings();
+        findLongest();
+    }
+
+    private static void findLongest() {
+        List<String> words = Arrays.asList("Java", "Stream", "API", "Development");
+
+        // With Sorting method
+
+        words.stream()
+                .sorted(Comparator.comparing(String::length).reversed())
+                .findFirst()
+                .ifPresent(s-> System.out.println("The longest word is "+s));
+
+        // Without sorting method --> Using reduce
+
+        words.stream()
+                .collect(Collectors.reducing((a, b) -> a.length() > b.length() ? a : b))
+                .ifPresent(s -> System.out.println("The longest word is again " + s));
+    }
+
+    /**
+     * Reducing function is used to give one single value from stream of values
+     * First param is the identity -> the initial value to be given while reduction
+     * Second param is the function (BinaryOperator) to be used while reduction.
+     */
+    private static void findMaxWithoutSortingAndInbuiltMaxFunction() {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        Integer max = numbers.stream().collect(Collectors.reducing(0, (a,b) -> a > b ? a : b));
+        System.out.println("Max number is "+max);
+    }
+
+    private static void concatenateStrings() {
+        List<String> words = Arrays.asList("Stream", "API", "is", "powerful");
+        String word = words.stream().collect(Collectors.joining(" "));
+        System.out.println(word);
+    }
+
+    private static void groupByLength() {
+        List<String> words = Arrays.asList("Java", "Stream", "API", "Code", "Fun", "Java");
+        words.stream().collect(Collectors.groupingBy(s->s.length())).forEach((k,v)-> System.out.println(k+" = "+v));
+    }
+
+    private static void findDuplicates() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 2, 5, 1);
+        HashSet<Integer> set = new LinkedHashSet<>();
+        List<Integer> duplicates = numbers.stream().filter(i -> !set.add(i)).toList();
+        System.out.println("Duplicates are "+duplicates);
+    }
+
+    /**
+     * FOr using functions like sum(), avg(), max() etc --> convert stream to IntStream using mapToInt()
+     */
+    private static void sumOfNumbersInList() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println("Sum is "+numbers.stream().mapToInt(Integer::intValue).sum());
+    }
+
+    private static void findFirstNonRepeatedChatracterInString() {
+        String input = "swiswsjk";
+        Arrays.stream(input.split(""))
+                .collect(Collectors.groupingBy((s->s), LinkedHashMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .findFirst()
+                .ifPresent(s-> System.out.println("The first non repeating character is "+s.getKey()));
+    }
+
+    private static void countStringsWithSpecificPrefix() {
+        List<String> names = Arrays.asList("Alice", "Bob", "Annie", "Alex", "Charlie");
+        long t = names.stream().filter(s -> s.startsWith("A")).count();
+        System.out.println("The count of names starting with A is "+t);
+    }
+
+    /**
+     * Find max from array of Integers
+     */
+    private static void findMax() {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        numbers.stream().mapToInt(Integer::intValue).max().ifPresent(System.out::println);
+    }
+}
